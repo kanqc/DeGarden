89395edde0aefb689d047ee9b8fcb4f19e538506
voanhkhoa19102001 <voanhkhoa1977@gmail.com>
2022-10-06 22:10:25 +0700

    push page login,....

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import login from "./component/login/login-form";
+import Login from "./component/login/login";
+import Register from "./component/register/register";
+import Forgotpassword from "./component/forgotpassword/forgotpassword";
+import Otpmail from "./component/otpmail/otpmail";
+import Newpassword from "./component/newpassword/newpassword";
 
 function App() {
-  return {
-    <login></login>
-  };
+  return (
+    <div>
+      <Newpassword />
+    </div>
+  );
 }
 
 export default App;
